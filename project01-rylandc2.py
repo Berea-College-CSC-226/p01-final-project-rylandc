be23faeca304c9ca8de51989d4a3585bf3249e3b
rylandc <rylandc@berea.edu>
2024-12-11 21:00:56 -0500

    Created three new methods to manage the win label and game logic, outlined most of the required main game logic code.

--- project01-rylandc2.py
+++ project01-rylandc2.py
@@ -28,7 +28,7 @@ class Game:
 
         """
         self.root = tk.Tk()
-        self.root.minsize(width=300, height=300)
+        self.root.minsize(width=250, height=80)
         self.root.maxsize(width=300, height=300)
         self.root.title("Game of NIM")
 
@@ -36,11 +36,18 @@ class Game:
         self.myTextBox1 = tk.Entry(self.root)
         self.myTextLabel1 = None
         self.labeltext = tk.StringVar()
+        self.win_label = tk.StringVar()
 
         self.bells = 0
         self.win = ""
-    
+
+
     def game_setup(self):
+        """
+        Offers to explain how to play the game.
+        Sets up the number of bells.
+
+        """
         print("Welcome to the Game of NIM!")
         y = False  # this is to determine if the random bells instruction is needed.
         instruction_loop = True
@@ -56,16 +63,13 @@ class Game:
                       "If there is no optimal number, it will choose a random number.")
                 print("You can either choose how many bells start in the basket...\n"
                       "...or a pseudorandom number from 16-31 inclusive will be selected.")
-                # print("THE INSTRUCTIONS HAVE PRINTED.")
                 y = True
                 instruction_loop = False
             elif choice == "N":
-                # print("THE INSTRUCTIONS WILL NOT BE PRINTED.")
                 instruction_loop = False
             else:
                 print("Sorry, please print 'Y' or 'N'.")
                 instruction_loop = True
-        # print("THE WHILE LOOP HAS BEEN EXITED.")
         bells_loop = True
         while bells_loop:  # set the number of bells loop
             choice = input("Would you like to set the starting number of bells yourself? Y/N: ")
@@ -82,6 +86,7 @@ class Game:
             else:
                 print("Sorry, please print 'Y' or 'N'.")
                 bells_loop = True
+        self.create_label1()
         print("Alright! Time to begin playing.\n"
               "Are you ready?\n"
               ".\n"
@@ -89,29 +94,21 @@ class Game:
               ".\n"
               "Not that it matters anyway.")
 
+
     def wn_setup(self):
         self.create_button1()
         self.create_textbox1()
-        self.create_label1()
         self.game_setup()
+
 
     def create_button1(self):
         self.myButton1 = tk.Button(self.root, text="Take bells", command=self.button1_handler)
         self.myButton1.grid(row=0, column=0) # top of the window
 
+
     def create_textbox1(self):
         self.myTextBox1.grid(row=1, column=0) # middle of the window
 
-    def get_entry(self):
-        """
-        Retrieves the textbox entry, to be called in NIM.run().
-        Only works if it's a number between 1 and 4 inclusive.
-
-        :return: what the user entered in the textbox
-        """
-        text = self.myTextBox1.get()
-        if 1 <= text <= 4: # if the number entered is between 1 and 4 inclusive
-            return text
 
     def create_label1(self):
         """
@@ -123,6 +120,44 @@ class Game:
         self.myTextLabel1 = tk.Label(self.root, textvariable= self.labeltext)
         self.myTextLabel1.grid(row=2, column=0) # bottom of the window
 
+
+    def create_win_label(self):
+        """
+        Creates a label to declare the winner when the number of bells drops to 0.
+
+        """
+        win_text = str(self.win)
+        self.win_label.set(value= win_text)
+        self.win_label = tk.Label(self.root, textvariable=self.win_label)
+        self.win_label.grid(row=2, column=1)  # right of the window
+
+
+    def comp_plays(self, text):
+        five = 5 - (self.bells % 5)
+        if five == 5 or five == 0:
+            take = int(random.randrange(1, 5))
+        else:
+            take = int(five)
+        self.bells -= take
+        self.create_label1()
+        if self.bells <= 1:
+            self.win = "You won!"
+            self.create_win_label()
+
+
+    def human_plays(self, text):
+        if 1 <= text <= 4:  # if the number entered is between 1 and 4 inclusive
+            self.bells -= text
+            if self.bells >= 1:
+                self.create_label1()
+                comp_turn = True
+            elif self.bells < 1:
+                self.win = "You lost!"
+                self.create_win_label()
+        else:
+            print("Whoops! Invalid entry!")
+
+
     def button1_handler(self):
         """
         Event handler for button1 above.
@@ -132,27 +167,10 @@ class Game:
         """
         text = self.myTextBox1.get()
         text = int(text)
-        #print(type(text))
-        if 1 <= text <= 4:  # if the number entered is between 1 and 4 inclusive
-            self.bells -= text
-            print(self.bells)
-            comp_turn = True
-            if self.bells <= 1:
-                pass
-
-        else:
-            print("Whoops! Invalid entry!")
-        
+        comp_turn = False
+        self.human_plays(text)
         if comp_turn:
-            five = 5 - (self.bells % 5)
-            # print(type(bells))
-            if five == 5 or five == 0:
-                take = int(random.randrange(1, 5))
-            else:
-                take = int(five)
-            # print(type(take))
-            self.bells -= take
-            print(self.bells)
+            self.comp_plays(text)
 
 
 
